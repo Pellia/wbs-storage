9136a856430ff1c9aa73272ddc588c37a045c7b3
Rupinder Singh <24740143+Pellia@users.noreply.github.com>
2025-01-09 15:52:28 +0100

    update ex2

--- ex2/script.js
+++ ex2/script.js
@@ -2,44 +2,87 @@
 const itemList = document.querySelector("ul");
 const userInput = document.getElementById("userInput");
 
-// Submit
-document.querySelector("button").addEventListener("click", function (event) {
+// Functions
+// Generate random UUID
+function generateUUID() {
+    const uuid = self.crypto.randomUUID();
+    return "task-" + uuid.replaceAll("-", "");
+}
+
+// Store Data
+function storeData(e) {
     const userValue = userInput.value.trim();
 
-    // Truthy Value
     if (userValue) {
         // Store Data
         const storedArray = JSON.parse(localStorage.getItem("myArray")) || [];
-        const newArray = [userValue, ...storedArray];
+        const uuid = generateUUID();
+        const newArray = [{ id: uuid, content: userValue }, ...storedArray];
         localStorage.setItem("myArray", JSON.stringify(newArray));
-
-        // Add new data to list
-        itemList.innerHTML = "";
-        newArray.forEach((item) => {
-            const newItem = document.createElement("li");
-            newItem.textContent = item;
-            itemList.appendChild(newItem);
-        });
 
         // Reset input field
         userInput.value = "";
     } else {
-        event.preventDefault();
+        e.preventDefault();
         alert("Please enter a value");
     }
+}
+
+// Delete Item
+function deleteItem(id) {
+    const storedArray = JSON.parse(localStorage.getItem("myArray")) || [];
+    const newStorage = storedArray.filter((item) => {
+        if (item.id !== id) {
+            return item;
+        }
+    });
+    localStorage.setItem("myArray", JSON.stringify(newStorage));
+    reloadPage();
+}
+
+// Reload Page
+function reloadPage() {
+    location.reload();
+}
+
+// Submit
+document.querySelector("button").addEventListener("click", (e) => {
+    storeData(e);
+    // e.preventDefault();
+    // const userValue = userInput.value.trim();
+    // if (userValue) {
+    //     // Store Data
+    //     const storedArray = JSON.parse(localStorage.getItem("myArray")) || [];
+    //     const uuid = generateUUID();
+    //     const newArray = [{ id: uuid, content: userValue }, ...storedArray];
+    //     localStorage.setItem("myArray", JSON.stringify(newArray));
+    //     // Reset input field
+    //     userInput.value = "";
+    // } else {
+    //     e.preventDefault();
+    //     alert("Please enter a value");
+    // }
 });
 
 // Reload
-document.getElementById("reload").addEventListener("click", () => {
-    location.reload();
-});
+document.getElementById("reload").addEventListener("click", reloadPage);
 
 // IIFE
 (() => {
     const storedArray = JSON.parse(localStorage.getItem("myArray")) || [];
     storedArray.forEach((item) => {
         const newItem = document.createElement("li");
-        newItem.textContent = item;
-        itemList.appendChild(newItem);
+        const delBtn = document.createElement("button");
+
+        newItem.textContent = item.content;
+        newItem.setAttribute("id", item.id);
+
+        delBtn.textContent = "Delete";
+        delBtn.classList.add("bg-red-500", "rounded", "mt-5", "text-white", "px-2");
+        delBtn.addEventListener("click", () => deleteItem(item.id));
+
+        newItem.append(delBtn);
+        itemList.append(newItem);
+        itemList.classList.add("flex", "flex-col");
     });
 })();
